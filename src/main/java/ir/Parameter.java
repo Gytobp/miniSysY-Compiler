a7701ef4da1f4824a025e0f5b7df7edbf3757e51
Gytobp <62336349+Gytobp@users.noreply.github.com>
2022-01-14 14:28:15 +0800

    architecture; part1: main done;

---
+++ src/main/java/ir/Parameter.java
@@ -0,0 +1,13 @@
+package ir;
+
+public class Parameter extends Value{
+    public Parameter(Value.Type type, String name) {
+        super(type);
+        super.setName(name);
+    }
+
+    @Override
+    public String toString() {
+        return getType().string + " " + getName();
+    }
+}
